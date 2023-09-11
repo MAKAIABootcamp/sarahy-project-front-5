c1803ea6ef7ba288dea944dd857a44cb0a9ad4b0
Maria Paulina Parra <mariapaulinap0531@gmail.com>
2023-09-11 11:21:28 -0500

    Creacion pagina perfil y footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Profile from './components/profile/profile.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>)
+  // <Provider store={store}>
+  //   <App />
+  // </Provider>
+  <Profile/>
+  )
  
