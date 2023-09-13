c09c7f301604a9802ac944c5e32262c2c7cfb785
sebastianbarrerah <sebasbh2008@hotmail.com>
2023-09-13 15:58:20 -0500

    agregando estilos

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import React from 'react'
-import Home from '../components/home/Home'
+import Home from '../components/home/Home';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/footer';
 import Wedding from '../components/wedding/Wedding';
+import Allies from '../components/allies/Allies';
+import Price from '../components/price/Price';
 
 
 
@@ -15,9 +17,12 @@ function Router() {
       <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="bodas" element={<Wedding />}/>
+          <Route path="/aliados" element={<Allies />}/>
+          <Route path="/cotizacion" element={<Price />}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
   )
 } 
 export default Router;
+
