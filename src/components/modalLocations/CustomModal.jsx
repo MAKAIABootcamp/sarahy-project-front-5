80111e84ef40ecc88f2fba8f08d5318eafae834a
sebastianbarrerah <sebasbh2008@hotmail.com>
2023-10-03 05:40:00 -0500

    modal locaciones

--- src/components/modalLocations/CustomModal.jsx
+++ src/components/modalLocations/CustomModal.jsx
@@ -44,12 +44,12 @@ const CustomModal = ({ isOpen, onRequestClose, data }) => {
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
       >
-        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/6672/3_2/960/jpg/img-20200130-wa0030_10_106672-166249390749574.jpeg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/5632/3_2/960/jpeg/whatsapp-image-2022-07-19-at-10-02-04-am_10_145632-165824304121413.jpeg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/6672/3_2/960/jpg/img-20190622-wa0025_10_106672-166249419933345.jpeg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://www.tusanagustin.com/assets/media/landings-renovadas/002-salones-de-fiestas-medellin/salones-de-fiestas-medellin-tu-san-agustin-slider-principal-new.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/8665/3_2/960/jpg/dsc-2758_10_128665-165056101221390.jpeg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/8665/3_2/960/jpg/img-0022_10_128665-165067291251017.jpeg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/6672/3_2/960/jpg/img-20200130-wa0030_10_106672-166249390749574.jpeg" alt="" className='imagen__slider--modal' /></SwiperSlide>
+        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/5632/3_2/960/jpeg/whatsapp-image-2022-07-19-at-10-02-04-am_10_145632-165824304121413.jpeg" alt="" className='imagen__slider--modal'/></SwiperSlide>
+        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/6672/3_2/960/jpg/img-20190622-wa0025_10_106672-166249419933345.jpeg" alt="" className='imagen__slider--modal' /></SwiperSlide>
+        <SwiperSlide><img src="https://www.tusanagustin.com/assets/media/landings-renovadas/002-salones-de-fiestas-medellin/salones-de-fiestas-medellin-tu-san-agustin-slider-principal-new.jpg" alt="" className='imagen__slider--modal' /></SwiperSlide>
+        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/8665/3_2/960/jpg/dsc-2758_10_128665-165056101221390.jpeg" alt="" className='imagen__slider--modal' /></SwiperSlide>
+        <SwiperSlide><img src="https://cdn0.matrimonio.com.co/vendor/8665/3_2/960/jpg/img-0022_10_128665-165067291251017.jpeg" alt="" className='imagen__slider--modal' /></SwiperSlide>
       </Swiper>
       <section className="informacion__modal">
         <div className='informacion__modal--titulo'>
@@ -58,7 +58,7 @@ const CustomModal = ({ isOpen, onRequestClose, data }) => {
           </h1>
           <img src={capacidad} alt="" className='imagen__modal--icono' />
           <span className='capacidad__modal'>
-            {elemento ? elemento.capacidad : ''}
+            {elemento ? elemento.capacidad : ''} Personas
           </span>
         </div>
         {/* <iframe className='informacion__modal--mapa' src={elemento ? elemento.html : ''} >
